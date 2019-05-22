1a86682e5a5c151122e68d2afe9b99c4a776d713
Francois-xavier BABIN <fbabin@e2r9p18.42.fr>
2019-05-22 21:08:04 +0200

    fixed top level function number to 1

--- d01/ex09/leibniz_pi.ml
+++ d01/ex09/leibniz_pi.ml
@@ -1,11 +1,11 @@
-let absolute_float (n: float) =
-  if n < 0.0 then -.n
-  else n
-
 let leibniz_pi (delta: float) = 
   if delta < 0.0 then -1
   else begin 
     let pi = (4.0 *. (atan 1.0)) in
+    let absolute_float (n: float) =
+      if n < 0.0 then -.n
+      else n
+    in
     let rec sum_leibniz_pi lower acc = 
       if (absolute_float (acc -. pi)) <= delta then lower
       else sum_leibniz_pi (lower + 1) (acc +. (4.0 *. ( (-1.0 ** (float_of_int lower)) /. (2.0 *. (float_of_int lower) +. 1.0))))
@@ -15,7 +15,7 @@ let leibniz_pi (delta: float) =
 let main () =
   print_int(leibniz_pi (-1.0));
   print_endline("");
-  print_int(leibniz_pi (0.0001));
+  print_int(leibniz_pi (0.0000001));
   print_endline("")
 
 let () = main ()
