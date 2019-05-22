1e7e13997c00630ed64f95f98399615ba50cb0f9
Francois-xavier BABIN <fbabin@e2r9p18.42.fr>
2019-05-22 15:26:39 +0200

    starting d02

---
+++ d02/ex01/crossover.ml
@@ -0,0 +1,25 @@
+let crossover lst1 lst2 =
+  let rec encode_aux nb acc = function
+    | [] -> []
+    | [e] -> acc @ [(nb + 1, e)]
+    | prev :: (next :: _ as tail) -> if prev = next then encode_aux (nb + 1) acc tail
+                                     else encode_aux 0 (acc @ [(nb + 1, prev)]) tail
+  in encode_aux 0 []
+
+let rec print_tuples =
+function
+| [] -> print_string("END \n")
+| (a, b) :: rest ->
+  Printf.printf "%i, %s; " a b;
+  print_tuples rest
+
+let main () =
+print_tuples [(3, "toto");(1, "a")];
+print_tuples (encode []);
+print_tuples (encode ["a"]);
+print_tuples (encode ["a"; "a"]);
+print_tuples (encode ["a"; "a"; "a"]);
+print_tuples (encode ["a"; "a"; "a"; "a"]);
+print_tuples (encode ["a"; "a"; "a"; "a"; "b"; "b"; "d"])
+
+let () = main ()
