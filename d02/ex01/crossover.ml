debd3ac99a40c272a1a98157e6c3b5e71fac7612
Francois-xavier BABIN <fbabin@e2r8p16.42.fr>
2019-05-24 21:32:24 +0200

    fixed ex01 error

--- d02/ex01/crossover.ml
+++ d02/ex01/crossover.ml
@@ -8,10 +8,9 @@ let contains elem =
 
 let crossover lst1 lst2 =
   let rec aux acc = function
-    | [] -> []
-    | [e] -> acc @ [e]
+    | [] -> acc
     | head :: tail ->
-        if (contains head lst2) && (contains head acc) == false then aux (acc @ [head]) tail
+        if (contains head lst2) && not (contains head acc) then aux (acc @ [head]) tail
         else aux acc tail
     in aux [] lst1 
 
@@ -24,6 +23,8 @@ let rec print_list =
 
 let main () =
   print_endline(string_of_bool (contains "d" ["a"; "b"]));
-  print_list(crossover ["c"; "a"] ["a"; "b"; "c"])
+  print_list(crossover ["c"; "a"] ["a"; "b"; "c"]);
+  print_list(crossover [] ["a"; "b"; "c"]);
+  print_list(crossover ["c"; "a"] [])
 
 let () = main ()
